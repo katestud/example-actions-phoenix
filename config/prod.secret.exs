de21c092159c6d19ccc31bde75660c87164c90dd
Kate Studwell <katestud@github.com>
2019-10-02 16:54:37 -0400

    Get env vars in an elixir-y way

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,13 +4,13 @@ use Mix.Config
 # you likely want to automate and keep it away from
 # your version control system.
 config :actions, ActionsWeb.Endpoint,
-  secret_key_base: ENV["SECRET_KEY_BASE"]
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
 config :actions, Actions.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: ENV["DB_USERNAME"],
-  password: ENV["DB_PASSWORD"],
+  username: System.get_env("DB_USERNAME"),
+  password: System.get_env("DB_PASSWORD"),
   database: "database_name",
   size: 20 # The amount of database connections in the pool
 
