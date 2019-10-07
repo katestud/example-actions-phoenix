55561564009f26441d8c59deb35da4ad7d1fe603
Kate Studwell <katestud@github.com>
2019-10-07 12:46:31 -0700

    Update host with full URL

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :actions, ActionsWeb.Endpoint,
   load_from_system_env: true,
-  url: [scheme: "https", host: "example_actions_phoenix", port: 443],
+  url: [scheme: "https", host: "example_actions_phoenix.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
